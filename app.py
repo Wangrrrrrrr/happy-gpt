0e8a39f62b890a409c03b92a25cfb92a99b82ab6
Wangrrrrrrr <133231438+Wangrrrrrrr@users.noreply.github.com>
2023-05-18 21:41:43 +0800

    Update app.py

--- app.py
+++ app.py
@@ -24,6 +24,7 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    if msg == "午餐吃什麼" || "晚餐吃什麼"
     lunch_options = ['便當', '麵類', '飯類', '燉飯', '三明治']
     #message = TextSendMessage(text=event.message.text)
     message = TextSendMessage(text=random.choice(lunch_options))
